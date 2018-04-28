e1805271912dc32ce0ed367d34d8d2b1298683cb
MarceloMansilla <mansillamarcelo90@gmail.com>
2018-04-27 23:05:08 -0300

    First Commit

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -15,6 +15,8 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
+        
+        
 
         $app->make(Kernel::class)->bootstrap();
 
