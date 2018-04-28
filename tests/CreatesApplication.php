a5ba3dbd780bff61592f30fbbf7b0c44e4e26b5b
MarceloMansilla <mansillamarcelo90@gmail.com>
2018-04-27 23:13:28 -0300

    Second commit

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -15,8 +15,6 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
-        
-        
 
         $app->make(Kernel::class)->bootstrap();
 
